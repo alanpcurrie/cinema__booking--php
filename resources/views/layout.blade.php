49bb5dd344ee880756bb8067b3677fefdadd66d4
usedfuturedigital <alanc909@gmail.com>
2017-06-06 21:25:46 +0100

    :sparkles:updated styles and views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,7 @@
 		    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 		  })(document);
 		</script>
-
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="{{elixir('css/app.css')}}" media="screen" title="no title">
 
 	</head>
